13f4cad90854e2c2205330d383eb95c482f8197a
Jake Schuler <jschuler@imail.wdym.info>
2025-09-08 19:40:16 +0000

    added support for custom colors in screen path

--- handlers/screen.go
+++ handlers/screen.go
@@ -9,16 +9,27 @@ import (
 func ScreenHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		view := c.Query("view")
+		backgroundColor := c.Query("background")
+		if backgroundColor == "" {
+			switch view {
+			case "queue":
+				backgroundColor = "0a0f3c"
+			default:
+				backgroundColor = "FF00FF"
+			}
+		}
 		switch view {
 		case "queue":
 			c.HTML(200, "upcoming_queue.tmpl", gin.H{
-				"title":      "FLL Event Screen - Queue Screen",
-				"event_name": os.Getenv("EVENT_NAME"),
+				"title":            "FLL Event Screen - Queue Screen",
+				"event_name":       os.Getenv("EVENT_NAME"),
+				"background_color": backgroundColor,
 			})
 		default:
 			c.HTML(200, "screen.tmpl", gin.H{
-				"title":      "FLL Event Screen - Public Screen",
-				"event_name": os.Getenv("EVENT_NAME"),
+				"title":            "FLL Event Screen - Public Screen",
+				"event_name":       os.Getenv("EVENT_NAME"),
+				"background_color": backgroundColor,
 			})
 		}
 	}
